f26c443adef57b8074e916bfc5335fdba782ed4f
Thomas Villa <thomvil87@gmail.com>
2017-10-20 12:18:28 +0200

    Initial commit

---
+++ aoc_2015/src/day25.rs
@@ -0,0 +1,15 @@
+const INPUT: &str = include_str!("../inputs/day25.txt");
+
+fn main() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn some_test() {
+        assert_eq!(2, 1 + 1);
+    }
+}
---
+++ aoc_2017/src/day03.rs
@@ -0,0 +1,15 @@
+const INPUT: &str = include_str!("../inputs/day03.txt");
+
+fn main() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn some_test() {
+        assert_eq!(2, 1 + 1);
+    }
+}
