6f3f1ec3345563cb26826b1589fd05efac34874a
Thomas Villa <thomvil87@gmail.com>
2017-10-24 16:29:31 +0200

    2016, day 8
    implementation

--- src/aoc_2016/day08/mod.rs
+++ src/aoc_2016/day08/mod.rs
@@ -1,4 +1,23 @@
-pub fn report(_instr: &str) {
+mod magnetic_strip;
+mod input_parser;
+
+use aoc_2016::day08::Instruction::*;
+use aoc_2016::day08::input_parser::*;
+use aoc_2016::day08::magnetic_strip::MagneticStrip;
+
+#[derive(Debug)]
+pub enum Instruction {
+    Rect(usize, usize),
+    RotateRow(usize, u32),
+    RotateColumn(usize, u32),
+}
+
+pub fn report(instr: &str) {
     println!(" -- 2016: Day 8 -- ");
+    let mut ms = MagneticStrip::new(6, 50);
+    ms.run(&parse_instructions(instr));
+    println!("| The display has {} lights lit.", ms.brightness());
+    println!("| The message reads:");
+    ms.print();
     println!(" ------------------");
 }
