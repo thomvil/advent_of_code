7b56c0e3c42d365b86d0b04f43c80e992b1f3598
Thomas Villa <thomvil87@gmail.com>
2017-10-24 11:35:45 +0200

    2016, day 5
    implementation

---
+++ src/aoc_2016/day05/door_hacker.rs
@@ -0,0 +1,70 @@
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+
+pub struct DoorHacker<'a> {
+    door_id: &'a str,
+    hasher: Md5,
+}
+impl<'a> DoorHacker<'a> {
+    pub fn new(id: &'a str) -> DoorHacker<'a> {
+        DoorHacker {
+            door_id: id,
+            hasher: Md5::new(),
+        }
+    }
+
+    fn hash(&mut self, idx: u32) -> String {
+        let input = format!("{}{}", self.door_id, idx);
+        self.hasher.input_str(&input);
+        let res = self.hasher.result_str();
+        self.hasher.reset();
+        res
+    }
+
+    fn code_digit_for_index(&mut self, idx: u32) -> Option<char> {
+        let hash_output = self.hash(idx);
+        match &hash_output[0..5] {
+            "00000" => Some(hash_output.chars().nth(5).unwrap()),
+            _ => None,
+        }
+    }
+
+    fn code_digit_for_index_v2(&mut self, idx: u32) -> Option<(usize, char)> {
+        let hash_output = self.hash(idx);
+        match &hash_output[0..5] {
+            "00000" => {
+                let location_char = hash_output.chars().nth(5).unwrap();
+                let location_digit: usize =
+                    location_char.to_string().parse().ok().or(Some(8)).unwrap();
+                if location_digit < 8 {
+                    let code_char = hash_output.chars().nth(6).unwrap();
+                    Some((location_digit, code_char))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    pub fn door_code_v2(&mut self) -> String {
+        let mut res = vec![None; 8];
+        let mut idx = 1;
+        while res.iter().any(|digits| digits.is_none()) {
+            self.code_digit_for_index_v2(idx)
+                .map(|(i, c)| res[i] = res[i].or_else(|| Some(c)));
+            idx += 1;
+        }
+        res.iter().map(|opt| opt.unwrap()).collect()
+    }
+
+    pub fn door_code(&mut self) -> String {
+        let mut idx = 1;
+        let mut res = String::new();
+        while res.len() < 8 {
+            self.code_digit_for_index(idx).map(|c| res.push(c));
+            idx += 1;
+        }
+        res
+    }
+}
--- src/aoc_2016/day05/mod.rs
+++ src/aoc_2016/day05/mod.rs
@@ -1,1 +1,28 @@
+mod door_hacker;
 
+use aoc_2016::day05::door_hacker::DoorHacker;
+
+pub fn report(instr: &str) {
+    println!(" -- 2016: Day 5 -- ");
+    let mut dh = DoorHacker::new(instr.trim());
+    println!("| Door code is:          {:?}", dh.door_code());
+    println!("| Improved door code is: {:?}", dh.door_code_v2());
+    println!(" ------------------");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    fn test_doorcode() {
+        let mut dh = DoorHacker::new("abc");
+        assert_eq!("18f47a30", dh.door_code());
+    }
+
+    // #[test]
+    fn test_doorcode_v2() {
+        let mut dh = DoorHacker::new("abc");
+        assert_eq!("05ace8e3", dh.door_code_v2());
+    }
+}
